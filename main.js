2601ad9bcbaa595fe022bad8535c3dc7adac30b5
Christhian Costa <christhiancosta27@gmail.com>
2022-07-12 15:11:13 -0300

    12/07 últimas atualizações
    adicionado "slider" dos comentários, retoques no sobre e em @media

--- main.js
+++ main.js
@@ -28,6 +28,124 @@ btnMobile.addEventListener('touchstart', toggleMenu);
 
 
 //=================CAROUSEL DEP================================================================
+var depoimentos = document.getElementsByClassName('comentario')
+var index = 0;
+
+    function deslizaComentario(){
+      index+=1;
+      if(index > (depoimentos.length-1)){
+        index=0;
+      }
+      comentSlide(index)
+    }
+
+    setInterval(deslizaComentario,9000)
+
+    function comentSlide(i){
+        var control = document.getElementsByClassName('undcontrole')
+        
+        index = i;
+        
+        if(i == 0){
+
+            control[0].style.backgroundColor = '#7D82B8'
+            control[1].style.backgroundColor = '#EDF2FB'
+            control[2].style.backgroundColor = '#EDF2FB'
+            control[3].style.backgroundColor = '#EDF2FB'
+            control[4].style.backgroundColor = '#EDF2FB'
+            control[5].style.backgroundColor = '#EDF2FB'
+
+            depoimentos[0].style.display = 'block'
+            depoimentos[1].style.display = 'none'   
+            depoimentos[2].style.display = 'none'
+            depoimentos[3].style.display = 'none'
+            depoimentos[4].style.display = 'none'
+            depoimentos[5].style.display = 'none'
+        }
+        if(i == 1){
+            control[0].style.backgroundColor = '#EDF2FB'
+            control[1].style.backgroundColor = '#7D82B8'
+            control[2].style.backgroundColor = '#EDF2FB'
+            control[3].style.backgroundColor = '#EDF2FB'
+            control[4].style.backgroundColor = '#EDF2FB'
+            control[5].style.backgroundColor = '#EDF2FB'
+
+
+            depoimentos[0].style.display = 'none'
+            depoimentos[1].style.display = 'block'   
+            depoimentos[2].style.display = 'none'
+            depoimentos[3].style.display = 'none'
+            depoimentos[4].style.display = 'none'
+            depoimentos[5].style.display = 'none'
+
+          }if(i == 2){
+            control[0].style.backgroundColor = '#EDF2FB'
+            control[1].style.backgroundColor = '#EDF2FB'
+            control[2].style.backgroundColor = '#7D82B8'
+            control[3].style.backgroundColor = '#EDF2FB'
+            control[4].style.backgroundColor = '#EDF2FB'
+            control[5].style.backgroundColor = '#EDF2FB'
+
+            depoimentos[0].style.display = 'none'
+            depoimentos[1].style.display = 'none'
+            depoimentos[2].style.display = 'block'
+            depoimentos[3].style.display = 'none'
+            depoimentos[4].style.display = 'none'
+            depoimentos[5].style.display = 'none'
+
+        }if(i == 3){
+            control[0].style.backgroundColor = '#EDF2FB'
+            control[1].style.backgroundColor = '#EDF2FB'
+            control[2].style.backgroundColor = '#EDF2FB'
+            control[3].style.backgroundColor = '#7D82B8'
+            control[4].style.backgroundColor = '#EDF2FB'
+            control[5].style.backgroundColor = '#EDF2FB'
+
+
+            depoimentos[0].style.display = 'none'
+            depoimentos[1].style.display = 'none'
+            depoimentos[2].style.display = 'none'
+            depoimentos[3].style.display = 'block'
+            depoimentos[4].style.display = 'none'
+            depoimentos[5].style.display = 'none'
+
+        }if(i == 4){
+          control[0].style.backgroundColor = '#EDF2FB'
+          control[1].style.backgroundColor = '#EDF2FB'
+          control[2].style.backgroundColor = '#EDF2FB'
+          control[3].style.backgroundColor = '#EDF2FB'
+          control[4].style.backgroundColor = '#7D82B8'
+          control[5].style.backgroundColor = '#EDF2FB'
+
+
+          depoimentos[0].style.display = 'none'
+          depoimentos[1].style.display = 'none'
+          depoimentos[2].style.display = 'none'
+          depoimentos[3].style.display = 'none'
+          depoimentos[4].style.display = 'block'
+          depoimentos[5].style.display = 'none'
+
+      }if(i == 5){
+        control[0].style.backgroundColor = '#EDF2FB'
+        control[1].style.backgroundColor = '#EDF2FB'
+        control[2].style.backgroundColor = '#EDF2FB'
+        control[3].style.backgroundColor = '#EDF2FB'
+        control[4].style.backgroundColor = '#EDF2FB'
+        control[5].style.backgroundColor = '#7D82B8'
+
+
+        depoimentos[0].style.display = 'none'
+        depoimentos[1].style.display = 'none'
+        depoimentos[2].style.display = 'none'
+        depoimentos[3].style.display = 'none'
+        depoimentos[4].style.display = 'none'
+        depoimentos[5].style.display = 'block'
+
+    }
+
+        index = i
+        console.log(i)
+    }
 
 // ==================CAROUSEL EST===============================================================
 const slides = document.getElementById("gancho")
